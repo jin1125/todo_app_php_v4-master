882a8bfbc2d8900f920a95d64a817955e96a7fb4
jin1125 <shogo.jinta@gmail.com>
2021-11-20 00:46:55 +0900

    12

--- work/app/config.php
+++ work/app/config.php
@@ -2,24 +2,21 @@
 
 session_start();
 
-define('DSN','mysql:host=db;dbname=myapp;charset=utf8mb4');
-define('DB_USER','myappuser');
-define('DB_PASS','myapppass');
-define('SITE_URL','http://'.$_SERVER['HTTP_HOST']);
+define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
+define('DB_USER', 'myappuser');
+define('DB_PASS', 'myapppass');
+define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
 
-
-// require_once(__DIR__.'/Utils.php');
-// require_once(__DIR__.'/Token.php');
-// require_once(__DIR__.'/Database.php');
-// require_once(__DIR__.'/Todo.php');
-
-spl_autoload_register(function($class){
-  $filename = sprintf(__DIR__.'/%s.php',$class);
-  
-  if(file_exists($filename)){
-    require($filename);
-  }else{
-    echo 'File not found:' . $fileName;
-    exit;
+spl_autoload_register(function ($class) {
+  $prefix = 'MyApp\\';
+  if (strpos($class, $prefix) === 0){
+    $fileName = sprintf(__DIR__.'/%s.php',substr($class,strlen($prefix)));
+    
+    if(file_exists($fileName)){
+      require($fileName);
+    }else{
+      echo 'File not found: ' . $fileName;
+      exit;
+    }
   }
 });
