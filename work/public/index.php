ac25a17beddf3e13c98df02130f4de3b43467439
jin1125 <shogo.jinta@gmail.com>
2021-11-19 16:47:38 +0900

    10

--- work/public/index.php
+++ work/public/index.php
@@ -21,6 +21,10 @@ try{
   exit;
 }
 
+function h($str){
+ return htmlspecialchars($str,ENT_QUOTES,'UTF-8');
+}
+
 function getTodos($pdo){
   $stmt = $pdo->query("SELECT * FROM todos ORDER BY id DESC");
   $todos = $stmt->fetchAll();
@@ -28,8 +32,6 @@ function getTodos($pdo){
 }
 
 $todos = getTodos($pdo);
-var_dump($todos);
-exit;
 ?>
 
 <!DOCTYPE html>
@@ -37,24 +39,20 @@ exit;
 <head>
   <meta charset="utf-8">
   <title>My Todos</title>
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
   <h1>Todos</h1>
 
   <ul>
-    <li>
-      <input type="checkbox" checked>
-      <span class='done'>Title</span>
-    </li>
-    <li>
-      <input type="checkbox">
-      <span>Title</span>
-    </li>
-    <li>
-      <input type="checkbox">
-      <span>Title</span>
-    </li>
+    <?php foreach($todos as $todo):?>
+      <li>
+        <input type="checkbox" <?= $todo->is_done ? 'checked':''; ?>>
+        <span class="<?= $todo->is_done ? 'done':''; ?>">
+        <?= h($todo->title); ?>
+      </span>
+      </li>
+    <?php endforeach;?>
   </ul>
 </body>
 </html>
