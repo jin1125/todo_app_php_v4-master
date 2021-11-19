9b26c0ccde8d2c86da12db72ac79a0e242d5db10
jin1125 <shogo.jinta@gmail.com>
2021-11-19 23:51:57 +0900

    8

--- work/app/Todo.php
+++ work/app/Todo.php
@@ -4,6 +4,7 @@ class Todo{
 
   public function __construct($pdo){
     $this->pdo = $pdo;
+    Token::create();
   }
 
   public function processPost(){
@@ -29,6 +30,40 @@ class Todo{
       header('Location: '.SITE_URL);
       exit;
     }
+  }
+
+  private function add(){  
+    $title = trim(filter_input(INPUT_POST,'title'));
+    if($title === ''){
+      return;
+    }
+  
+    $stmt = $this->pdo->prepare("INSERT INTO todos (title) VALUES (:title)");
+    $stmt -> bindValue('title',$title,PDO::PARAM_STR);
+    $stmt -> execute();
+  
+  }
+ 
+  private function toggle(){
+    $id = filter_input(INPUT_POST,'id');
+    if(empty($id)){
+     return;
+    }
+ 
+    $stmt = $this->pdo->prepare("UPDATE todos SET is_done = NOT is_done WHERE id = :id");
+    $stmt -> bindValue('id',$id,PDO::PARAM_INT);
+    $stmt -> execute();
+  }
+  
+  private function delete(){
+    $id = filter_input(INPUT_POST,'id');
+    if(empty($id)){
+     return;
+    }
+ 
+    $stmt = $this->pdo->prepare("DELETE FROM todos WHERE id = :id");
+    $stmt -> bindValue('id',$id,PDO::PARAM_INT);
+    $stmt -> execute();
   }
 
 
