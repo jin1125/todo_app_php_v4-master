4dc1d33a9ce726c0ccb03990323e7eb98183b12f
jin1125 <shogo.jinta@gmail.com>
2021-11-20 10:46:03 +0900

    4

--- work/public/js/main.js
+++ work/public/js/main.js
@@ -1,36 +1,35 @@
-'use strict'
+"use strict";
 {
   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-  checkboxes.forEach((checkbox)=>{
-    checkbox.addEventListener('change',()=>{
-      const url = '?action=toggle';
-      const options = {
-        method:"POST",
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", () => {
+      
+      fetch("?action=toggle", {
+        method: "POST",
         body: new URLSearchParams({
-          id:,
-          token:,
+        id: checkbox.dataset.id,
+        token: checkbox.dataset.id,
         }),
-      };
+      });
 
-      fetch(url,options);
-    })
-  })
+    });
+  });
 
-  const deletes = document.querySelectorAll('.delete');
-  deletes.forEach((span)=>{
-    span.addEventListener('click',()=>{
-      if(!confirm('Are you sure?')){
+  const deletes = document.querySelectorAll(".delete");
+  deletes.forEach((span) => {
+    span.addEventListener("click", () => {
+      if (!confirm("Are you sure?")) {
         return;
       }
       span.parentNode.submit();
-    })
-  })
+    });
+  });
 
-  const purge = document.querySelector('.purge');
-  purge.addEventListener('click',()=>{
-    if(!confirm('Are you sure?')){
+  const purge = document.querySelector(".purge");
+  purge.addEventListener("click", () => {
+    if (!confirm("Are you sure?")) {
       return;
     }
     purge.parentNode.submit();
-  })
+  });
 }
