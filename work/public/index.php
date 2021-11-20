bd2dd3a956b8f6cdc4f2e403d3f00646333769ee
jin1125 <shogo.jinta@gmail.com>
2021-11-20 12:23:32 +0900

    13

--- work/public/index.php
+++ work/public/index.php
@@ -37,21 +37,13 @@ $todos = $todo->getAll();
 
     <ul>
       <?php foreach($todos as $todo):?>
-        <li>
+        <li data-id='<?= Utils::h($todo->id) ?>'>
 
-          <input
-            type="checkbox" 
-            data-id='<?= Utils::h($todo->id) ?>'
-            <?= $todo->is_done ? 'checked':''; ?>>
+          <input type="checkbox" <?= $todo->is_done ? 'checked':''; ?>>
 
           <span><?= Utils::h($todo->title); ?></span>
 
-          <span
-            data-id="<?= Utils::h($todo->id); ?>"
-            class='delete'
-            >
-            x
-          </span>
+          <span class='delete'>x</span>
 
          </li>
 
